969afcd66b9200c68a798ed5a15a909a4aafc6d0
zhangjiawan <1876052204@qq.com>
2023-11-11 16:45:44 +0800

    PASS test case:PriceCaculator.should_return_100_when_give_cash_normal_and_price_100

--- tests/PriceCaculatorTest.cpp
+++ tests/PriceCaculatorTest.cpp
@@ -13,5 +13,5 @@ TEST(hello, should_return_100_when_give_cash_normal_and_price_100)
     double cash = priceCaculator.AcceptCash(DiscountType::CASS_NORMAL,100.0);
 
     // then
-    EXPECT_DOUBLE_EQ(0, cash);
+    EXPECT_DOUBLE_EQ(100, cash);
 }
