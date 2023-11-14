9fd23a2d603687aca56d2337349d1eb7bbafeb93
zhangjiawan <1876052204@qq.com>
2023-11-14 20:03:42 +0800

    PASS test:should_return_50_when_give_cash_back_and_price_50 and should_return_210_when_give_cash_back_and_price_250

--- tests/PriceCalculatorTest.cpp
+++ tests/PriceCalculatorTest.cpp
@@ -39,3 +39,27 @@ TEST(hello, should_return_80_when_give_cash_back_and_price_100)
     // then
     EXPECT_DOUBLE_EQ(80, cash);
 }
+
+TEST(hello, should_return_50_when_give_cash_back_and_price_50)
+{
+    // given
+    PriceCalculator priceCalculator;
+
+    // when
+    double cash = priceCalculator.AcceptCash(DiscountType::CASS_BACK,50.0);
+
+    // then
+    EXPECT_DOUBLE_EQ(50, cash);
+}
+
+TEST(hello, should_return_210_when_give_cash_back_and_price_250)
+{
+    // given
+    PriceCalculator priceCalculator;
+
+    // when
+    double cash = priceCalculator.AcceptCash(DiscountType::CASS_BACK,250.0);
+
+    // then
+    EXPECT_DOUBLE_EQ(210, cash);
+}
