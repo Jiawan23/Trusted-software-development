ad771004a126e82ba183cfa4901d4b0fe538562e
zhangjiawan <1876052204@qq.com>
2023-11-14 20:31:30 +0800

    PASS test:PriceCalculator.should_return_80_when_give_cash_percentoff_80_and_price_100 && should_return_70_when_give_cash_percentoff_70_and_price_100

--- src/PriceCalculator.cpp
+++ src/PriceCalculator.cpp
@@ -11,6 +11,12 @@ double PriceCal::PriceCalculator::AcceptCash(const DiscountType type, const doub
   case DiscountType::CASS_PERCENTOFF_90:
     cash = money * 0.9;
     break;
+  case DiscountType::CASS_PERCENTOFF_80:
+    cash = money * 0.8;
+    break;
+  case DiscountType::CASS_PERCENTOFF_70:
+    cash = money * 0.7;
+    break;
   case DiscountType::CASS_BACK:
     cash = money - 20 * (static_cast<int>(money) / 100 );
     break;
